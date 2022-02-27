7086dddd430c10cc15073ab7c1a4d75657a12578
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-27 17:03:46 +0300

    added default and advanced quick sort impl

---
+++ src/main/java/chapter4/quick_sort/QuickSort.java
@@ -0,0 +1,75 @@
+package chapter4.quick_sort;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> quickSort(List<Integer> list) {
+        if (list.size() < 2) {
+            // base case, arrays with 0 or 1 element are already "sorted"
+            return list;
+        } else {
+            // recursive case
+            Integer pivot = list.get(0);
+
+            // sub-array of all the elements less than the pivot
+            List<Integer> less = list.stream().skip(1).filter(el -> el <= pivot)
+                    .collect(Collectors.toList());
+
+            // sub-array of all the elements greater than the pivot
+            List<Integer> greater = list.stream().skip(1).filter(el -> el > pivot)
+                    .collect(Collectors.toList());
+
+            return Stream.of(
+                            quickSort(less).stream(),
+                            Stream.of(pivot),
+                            quickSort(greater).stream())
+                    .flatMap(Function.identity()).collect(Collectors.toList());
+        }
+    }
+
+
+    public static void defaultQuickSort(int[] array, int leftBorder, int rightBorder) {
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
+        int pivot = array[(leftMarker + rightMarker) / 2];
+        do {
+            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
+            while (array[leftMarker] < pivot) {
+                leftMarker++;
+            }
+            // Двигаем правый маркер, пока элемент больше, чем pivot
+            while (array[rightMarker] > pivot) {
+                rightMarker--;
+            }
+            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
+            if (leftMarker <= rightMarker) {
+                // Левый маркер будет меньше правого только если мы должны выполнить swap
+                if (leftMarker < rightMarker) {
+                    int tmp = array[leftMarker];
+                    array[leftMarker] = array[rightMarker];
+                    array[rightMarker] = tmp;
+                }
+                // Сдвигаем маркеры, чтобы получить новые границы
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+        // Выполняем рекурсивно для частей
+        if (leftMarker < rightBorder) {
+            defaultQuickSort(array, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightMarker) {
+            defaultQuickSort(array, leftBorder, rightMarker);
+        }
+    }
+}
